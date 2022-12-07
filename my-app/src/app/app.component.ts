d1e7bfbee40ef94624cd805c49cfe4ddda21c84d
Russell <russell.tchamba@gmail.com>
2022-12-07 14:55:09 +0100

    add image and slide

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  images = ['pexels-drew-williams-2657669.jpg','pexels-nao-triponez-792199.jpg','pexels-pixabay-459653.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() =>{
+      this.currentImage++;
+      this.currentImage = this.currentImage% this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+
+    },8000);
+    
+  }
+
+
+
 }
